ecbfff116c00c10178d9586cdadfa5682ae5c9cc
Amuke Sunday <sundayamuke7@gmail.com>
2020-10-10 00:50:31 +0100

    Moved MainActivity.kt into ui/

--- app/src/main/java/com/sundayamuke/findadev/ui/MainActivity.kt
+++ app/src/main/java/com/sundayamuke/findadev/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.sundayamuke.findadev
+package com.sundayamuke.findadev.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.sundayamuke.findadev.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
