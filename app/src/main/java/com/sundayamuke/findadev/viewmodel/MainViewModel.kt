185788b92cedef0aa79ccb43675c582002c8b796
Amuke Sunday <sundayamuke7@gmail.com>
2020-10-12 21:07:08 +0100

    Modified viewModel
    ...for testing

--- app/src/main/java/com/sundayamuke/findadev/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/sundayamuke/findadev/viewmodel/MainViewModel.kt
@@ -4,5 +4,13 @@ import androidx.lifecycle.ViewModel
 import com.sundayamuke.findadev.model.Dev
 
 class MainViewModel: ViewModel() {
-    val dev = Dev()
+    val devs = listOf<Dev>(
+        Dev(fullName = "Oluwafemi Juri", stack = 3),
+        Dev(fullName = "Mike King", stack = 5),
+        Dev(fullName = "Omilo Samuel", stack = 0),
+        Dev(fullName = "Tomisin", stack = 3),
+        Dev(fullName = "Tofunmi", stack = 3)
+    )
+
+
 }
