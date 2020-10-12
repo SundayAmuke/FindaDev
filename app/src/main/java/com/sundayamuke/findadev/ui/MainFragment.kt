5b1a25fccdc3b424f4eba6ffda0c3b12c12c835d
Amuke Sunday <sundayamuke7@gmail.com>
2020-10-12 21:31:51 +0100

    Refactored UI

--- app/src/main/java/com/sundayamuke/findadev/ui/MainFragment.kt
+++ app/src/main/java/com/sundayamuke/findadev/ui/MainFragment.kt
@@ -5,15 +5,45 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import com.sundayamuke.findadev.R
+import com.sundayamuke.findadev.adapters.ItemClickListener
+import com.sundayamuke.findadev.adapters.MainAdapter
+import com.sundayamuke.findadev.databinding.FragmentMainBinding
+import com.sundayamuke.findadev.model.Dev
+import com.sundayamuke.findadev.viewmodel.MainViewModel
 
 class MainFragment : Fragment() {
+
+    private lateinit var binding: FragmentMainBinding
+
+    private val viewModel: MainViewModel by lazy {
+        ViewModelProvider(this)[MainViewModel::class.java]
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
+
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_main, container, false)
+
+        binding.viewModel = viewModel
+
+        binding.recyclerView.adapter = MainAdapter(user = Dev(), clickListener)
+
+        binding.lifecycleOwner = this
+
+        return binding.root
     }
+
+    private val clickListener = ItemClickListener(
+        profileClickListener = {dev: Dev ->
+            Toast.makeText(context, dev.fullName, Toast.LENGTH_SHORT).show()
+        }, devClickListener = {dev: Dev ->
+            Toast.makeText(context, dev.fullName, Toast.LENGTH_SHORT).show()
+        }
+    )
 }
