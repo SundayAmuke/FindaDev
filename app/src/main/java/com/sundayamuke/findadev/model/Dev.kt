90f38de5bbaab28f3405843c0d03b3c22dd3c22b
Amuke Sunday <sundayamuke7@gmail.com>
2020-10-13 10:06:23 +0100

    Parcelized Dev model

--- app/src/main/java/com/sundayamuke/findadev/model/Dev.kt
+++ app/src/main/java/com/sundayamuke/findadev/model/Dev.kt
@@ -1,9 +1,13 @@
 package com.sundayamuke.findadev.model
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
 data class Dev(
     var fullName: String = "Amuke Sunday",
     var portfolioLink: String = "https://github.com/SundayAmuke",
     var stack: Int = 1,
     var jobType: Int = 1,
     var technologies: String = "Android, Java, Kotlin..."
-)
+) : Parcelable
