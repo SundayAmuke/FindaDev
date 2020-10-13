baec2b01e9429e472330946eb1129847fe2db642
Amuke Sunday <sundayamuke7@gmail.com>
2020-10-13 10:04:11 +0100

    Added another fragment for dev

---
+++ app/src/main/java/com/sundayamuke/findadev/ui/DevFragment.kt
@@ -0,0 +1,20 @@
+package com.sundayamuke.findadev.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.sundayamuke.findadev.R
+
+class DevFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_dev, container, false)
+    }
+
+}
